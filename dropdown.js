98fe0787b44b49918d23bcd2cda1be96b750bb62
Gonernyash <di.sviridovskiy@gmail.com>
2020-10-07 18:43:57 +0300

    first commit

---
+++ dropdown.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import "./dropdown.css"
+
+class Dropdown extends Component {
+    render() {
+        return (
+            <div className='dropdown'>
+
+            </div>
+        );
+    }
+} 
