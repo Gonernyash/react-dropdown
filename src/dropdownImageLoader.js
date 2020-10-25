cabb8ecbf4fe875a3471e7cfb200affcc98ed24d
Gonernyash <di.sviridovskiy@gmail.com>
2020-10-25 16:59:34 +0300

    optimising image loading

--- src/dropdownImageLoader.js
+++ src/dropdownImageLoader.js
@@ -1,12 +1,8 @@
-const importAll = (i) => i.keys().map(i);
-const getId = (img) => {
-    img = img.slice(14);
-    img = img.slice(0, -13);
-    return img + ".svg"
-}
+const importAll = (request) => request.keys().reduce((imagesObj, current) => {
+    imagesObj[current] = request(current);
+    return imagesObj
+}, {});
 
-let importImages = {};
-importAll(require.context("./flags", false, /.*\.svg$/))
-.forEach((val) => importImages[getId(val)] = val);
+const importImages = importAll(require.context("../images", false, /.*\.svg$/))
 
 export default importImages;
