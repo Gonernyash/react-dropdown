b9028f2b77b28507ed8fdeb6a396fa739b3a65cd
Gonernyash <di.sviridovskiy@gmail.com>
2020-10-27 18:49:54 +0300

    optimized icons loading and made it the js class

--- src/dropdownImageLoader.js
+++ src/dropdownImageLoader.js
@@ -1,8 +1,21 @@
-const importAll = (request) => request.keys().reduce((imagesObj, current) => {
-    imagesObj[current] = request(current);
-    return imagesObj
-}, {});
+class ImportImages {
+    constructor() {
+        this.list = this.import(require.context("../images", false, /.*\.svg$/))
+    }
 
-const importImages = importAll(require.context("../images", false, /.*\.svg$/))
+    import(request) {
+        const result = request.keys().reduce((imagesObj, current) => {
+            imagesObj[current] = request(current);
+            return imagesObj
+        }, {});
+        return result
+    }
 
-export default importImages;
+    getKey(value) {
+        for (let key in this.list) {
+            if (this.list[key] === value) return key
+        }
+    }
+}
+
+export default new ImportImages();
